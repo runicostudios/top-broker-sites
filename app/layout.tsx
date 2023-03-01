9f4edc6c25966a0883a25ecc757b2c9d6c738491
Brodie Clarke <brodieclarke97@gmail.com>
2023-03-01 23:13:11 +0400

    Added default font 'Lato'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import './globals.css';
+import { lato } from './fonts';
 
 export default function RootLayout({
   children,
@@ -6,13 +7,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang="en" className={lato.className}>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
