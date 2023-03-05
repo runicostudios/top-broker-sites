b31fdf0a98fa65d41ea2dd8ef7396f3d39e9f822
Brodie Clarke <brodieclarke97@gmail.com>
2023-03-05 04:40:59 +0400

    enabled experimental mode for nextjs 13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>test</h1>
-    </main>
-  );
+  return <main className={styles.main}></main>
 }
